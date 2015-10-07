a89769acb5b9b56d46e2dbb0ef2f24b3ebe8a262
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-10-07 16:53:13 -0500

    updated app.ini and added populator

---
+++ ittok/populate.py
@@ -0,0 +1,32 @@
+"""
+Populate contains two functions that are called on application startup
+(if you haven't modified kotti.populators).
+"""
+
+from pyramid.i18n import LocalizerRequestMixin
+from pyramid.threadlocal import get_current_registry
+
+from kotti import get_settings
+from kotti.resources import DBSession
+from kotti.resources import get_root
+from kotti.resources import Document
+
+from kotti.security import get_principals
+from kotti.security import SITE_ACL
+from kotti.util import _
+from kotti.workflow import get_workflow
+
+
+
+def populate():
+    """
+    Create the base page...
+    """
+    root = get_root()
+    tenyu = root.get('tenyu', Document())
+    tenyu.title = 'tenyu'
+    tenyu.default_view = 'tenyu'
+    root['tenyu'] = tenyu
+
+    
+    

cb3ae89307207473282f47ddb2265e444886b0f3
Joseph Rawson <joseph.rawson.works@littledebian.org>
2015-10-19 11:42:39 -0500

    added kotti addons

--- setup.py
+++ setup.py
@@ -8,16 +8,35 @@ with open(os.path.join(here, 'README.txt')) as f:
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
-requires = [
+install_requires = [
     'pyramid',
     'pyramid_chameleon',
-    'pyramid_debugtoolbar',
+    'pyramid_mako',
     'pyramid_tm',
     'SQLAlchemy',
     'transaction',
     'zope.sqlalchemy',
     'waitress',
+    'repoze.workflow>=master',
+    'Kotti>=master',
+    'kotti_tinymce',
+    'kotti_accounts',
+    'kotti_blog',
+    'kotti_calendar',
+    'kotti_jsonapi>=master',
+    'kotti_settings',
+    'kotti_velruse',
     ]
+
+development_requires = [
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'repoze.debug',
+    'repoze.tm2',
+    'repoze.who',
+    'repoze.what',
+    ]
+
 
 setup(name='ittok',
       version='0.0',
@@ -37,11 +56,21 @@ setup(name='ittok',
       include_package_data=True,
       zip_safe=False,
       test_suite='ittok',
-      install_requires=requires,
+      install_requires=install_requires,
       entry_points="""\
       [paste.app_factory]
       main = ittok:main
       [console_scripts]
       initialize_ittok_db = ittok.scripts.initializedb:main
       """,
+      dependency_links=[
+        'https://github.com/knowah/PyPDF2/archive/master.tar.gz#egg=PyPDF2-1.15dev',
+        'https://github.com/repoze/repoze.workflow/archive/master.tar.gz#egg=repoze.workflow-master',
+        'https://github.com/umeboshi2/trumpet/archive/master.tar.gz#egg=trumpet',
+        'https://github.com/umeboshi2/Kotti/archive/master.tar.gz#egg=Kotti-1.2.2-dev',
+        'https://github.com/umeboshi2/kotti_jsonapi/archive/master.tar.gz#egg=kotti_jsonapi-master',
+        ],
+      extras_require={
+          'development': development_requires,
+          },
       )
